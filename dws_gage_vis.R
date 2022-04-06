9f1bdc367ef6b1f62d37876310254b5faedd0198
David Kahler <david.m.kahler@gmail.com>
2022-04-06 12:21:47 -0400

    loaded both gages, concatenated

--- dws_gage_vis.R
+++ dws_gage_vis.R
@@ -7,7 +7,15 @@ library(lubridate)
 # install_github("LimpopoLab/hydrostats")
 library(hydrostats)
 
-x <- read_csv("A7H008.csv", col_names = FALSE)
+x <- read_csv("A7H004.csv", col_names = FALSE)
+x <- x %>%
+     rename(dt=X1,UNIX=X2,lev_m=X3,levQC=X4,flow_m3s=X5,flowQC=X6) # note UNIX date time is UTC
 
-ggplot(x) +
-  geom_point(aes(x=X1,y=X4))
+y <- read_csv("A7H008.csv", col_names = FALSE)
+y <- y %>%
+     rename(dt=X1,UNIX=X2,lev_m=X3,levQC=X4,flow_m3s=X5,flowQC=X6) # note UNIX date time is UTC
+
+z <- rbind(x,y)
+
+ggplot(z) +
+  geom_point(aes(x=dt,y=flow_m3s))
