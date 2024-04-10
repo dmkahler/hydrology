3f20e6747f97801a8a5c061ee778f5a0719ae621
David Kahler <david.m.kahler@gmail.com>
2024-04-10 12:35:51 -0400

    starting parallel practice

---
+++ parallel_time.py
@@ -0,0 +1,15 @@
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Done sleeping')
+
+do_something()
+do_something()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} seconds')
