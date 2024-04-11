725ae189f9f5c8f5f24c769bddc36a9ffdcb1d2c
David Kahler <david.m.kahler@gmail.com>
2024-04-10 21:57:41 -0400

    tried concurrent.futures - still not working

--- parallel_time.py
+++ parallel_time.py
@@ -2,25 +2,30 @@
 # based on: https://www.youtube.com/watch?v=fKl2JW_qrso
 
 import time
+import concurrent.futures
 import multiprocessing
 
 start = time.perf_counter()
 
-def do_something():
-    secs = 1.5
+def do_something(secs):
     print(f'Sleeping {secs} seconds...')
     time.sleep(secs)
-    print('Done sleeping')
+    return 'Done sleeping'
 
-processes = []
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    f1 = executor.submit(do_something, 1)
+    print(f1.result())
 
-for _ in range(10):
-    p = multiprocessing.Process(target = do_something)
-    p.start()
-    processes.append(p)
+# Multiprocessing toolbox - didn't work.
+# processes = []
 
-for process in processes:
-    process.join()
+# for _ in range(10):
+#     p = multiprocessing.Process(target = do_something)
+#     p.start()
+#     processes.append(p)
+
+# for process in processes:
+#     process.join()
 
 finish = time.perf_counter()
 
