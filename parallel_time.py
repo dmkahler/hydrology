82f936ada8b29b1756b69c824cb643bd8c816f14
David Kahler <david.m.kahler@gmail.com>
2024-04-12 17:12:33 -0400

    nope - error in example; trying again.

--- parallel_time.py
+++ parallel_time.py
@@ -1,6 +1,7 @@
 # Trying out parallel in python
 # based on: https://www.youtube.com/watch?v=fKl2JW_qrso - didn't work
-# based on: https://www.youtube.com/watch?v=GT10PnUFLlE 
+# based on: https://www.youtube.com/watch?v=GT10PnUFLlE - error in multiprocessing - no join, comparison was degenerate
+
 
 import time
 import multiprocessing as mp
@@ -21,12 +22,24 @@ def calc_c(numbers):
     for number in numbers:
         results_c.append(number**4)
 
+start = time.time()
+processes = []
+
 if __name__ == '__main__':
     number_list = list(range(100000))
-    start = time.perf_counter()
-    calc_a(number_list)
-    calc_b(number_list)
-    calc_c(number_list)
-    finish = time.perf_counter()
+
+    p1 = mp.Process(target = calc_a, args = (number_list,))
+    p2 = mp.Process(target = calc_b, args = (number_list,))
+    p3 = mp.Process(target = calc_c, args = (number_list,))
+
+    p1.start()
+    p2.start()
+    p3.start()
+
+finish = time.time()
 
 print(f'Finished in {round(finish-start, 3)} seconds')
+
+# for number list 1e5:
+# in order: 0.071 seconds
+# in multiprocess 
